dbf4290469112c03e080af7a22ff8c87b489714a
tranhph <86335812+tranhph@users.noreply.github.com>
2021-07-22 12:35:14 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,173 @@
-import './App.css';
+import './App.scss';
+import React, { useState, useEffect } from "react"
+import fetch from "isomorphic-fetch";
+
 import ArticleCard from './Components/ArticleCard/ArticleCard';
 
 function App() {
+  // parse json from server
+  const [infoTag, setInfoTag] = useState({})
+  const [articlesArray, setArticleArray] = useState([])
+  const [pageNumber, setPageNumber] = useState(1)
+
+  const HOST = "https://cms.vietcetera.dev/client/api"
+  const getTagAndItsRelates = `${HOST}/v2/tags/coi-mo`
+  const getTagsArticle = `${HOST}/v2/tags/articles/coi-mo?page=${pageNumber}&limit=8&languageSort=VN`;
+  const getTagsRecommendedArticles = `${HOST}/v2/tags/recommend-articles/coi-mo?page=${pageNumber}&limit=8`;
+
+  const getArticlesOfTag = () => {
+    // fetch của browser
+    // IE => ko có fetch
+    try {
+      // const response = await fetch(`https://cms.vietcetera.dev/client/api//v2/tags/tan-chay`)
+      // const json = response.json()
+      // console.log("json", json)
+      return fetch(getTagAndItsRelates)
+        .then((response) => response.json())
+        .then(json => {
+          if (json.message === "GET_SUCCESSFUL") {
+            setInfoTag(prevInfo => ({ ...prevInfo, ...json.data }))
+          }
+        })
+    }
+    catch (e) {
+      console.log("e", e)
+    }
+  }
+  const getDataArticles = () => {
+    try {
+      return fetch(getTagsArticle)
+        .then((response) => response.json())
+        .then(json => {
+          if (json.message === "GET_SUCCESSFUL") {
+            setArticleArray([...articlesArray, ...json.data.articles])
+            console.log(`button clicked, page is `, pageNumber)
+          }
+        })
+    }
+    catch (e) {
+      console.log("e", e)
+    }
+  }
+  useEffect(() => {
+    console.log("didmount run")
+    getArticlesOfTag()
+    getDataArticles()
+  }, [])
+
+  let maxArticlePages = infoTag.totalOfArticle / 8;
+  if ((infoTag.totalOfArticle % 8) !== 0 && infoTag.totalOfArticle > 8) maxArticlePages++;
+  const getNextArticle = () => {
+    if (pageNumber <= maxArticlePages) {
+      setPageNumber(pageNumber + 1)
+      getDataArticles()
+    }
+  }
+
   return (
     <div className="tagPageContainer">
-      <h1 className="tagName">#Văn Hóa</h1>
+      <h1 className="tagName">#{infoTag.name}</h1>
+      {infoTag.relatedTags && infoTag.relatedTags.length > 0 && <p className="relatedTagHeader">Related tags</p>}
 
-      <p className="relatedTagHeader">Related tags</p>
       <div className="relatedTags">
-        <a href="/" title="song xanh">#sống xanh</a>
-        <a href="/" title="doi song">#đời sống</a>
-        <a href="/" title="song dep">#sống đẹp</a>
-        <a href="/" title="phong cach">#phong cách</a>
+        {infoTag.relatedTags && infoTag.relatedTags.map((relatedTag) => {
+          return <a href={`/vn/tags/${relatedTag.slug}`} title={relatedTag.name} key={relatedTag.id}>#{relatedTag.name}</a>
+        })}
+        {/* {renderRelatedTags()} */}
       </div>
       <div className="articlesContainer">
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
+        {articlesArray && articlesArray.map((singleArticle) => {
+          return <ArticleCard articleDetails={singleArticle} key={singleArticle.id} />
+        })}
+
       </div>
       <div className="btnContainer">
-        <button><a title="Xem thêm bài viết" href="/">Xem thêm bài viết</a></button>
+        {pageNumber <= maxArticlePages && <button onClick={getNextArticle}>Xem thêm bài viết</button>}
       </div>
     </div>
   );
 }
 
 export default App;
+
+  // 1. pending
+  // 2. fulfilled
+  // 3. reject
+
+  // const sum = (a, b) => {
+  //   return a + b
+  // }
+
+  // const sum = (a,b) => a+b
+
+  // async await => higher promise => sinh ra để replace promise traditional
+  // promise => pure promise
+
+  // component re-render from 2nd times => phụ thuộc vào state của component
+
+  // this.setState({...this.state , article : res.json()})
+
+  // state = {
+  //   articles : []
+  // }
+
+  //  Didmount , didUpdate ,willUnMount => 3 life circle chính của react
+  // class component => reactjs
+
+  // conponentDidMount(){
+  // const articles = this.state.articles
+  // render sau khi component render xong
+  // only run 1 time after component rendered
+  // article = 8 bài
+  // }
+
+  // conponentDidUpdate(){
+  // phụ thuộc dependency để update cho component => xài phải hiểu tránh việc infinity loop
+  // article = 16 bài
+  // }
+
+  // conponentWillUnMount(){
+  // xử lý logic return về mảng rỗng
+  // only run 1 time after move out of component
+  // }
+
+
+  // re-render => cập nhật lại UI UX cho component => 2nd times
+  // function component => react hooks
+  // hooks have 3 props xài nhiều nhất useState , useEffect
+  // useState sinh ra replace state bên class
+  // useEffect sinh ra để replace lifes circle bên class
+  // didmount => only run 1 time after component rendered
+  // useEffect(() => {
+  //   console.log("didmount run")
+    // console.log("articles", articles)
+  //   getArticlesOfTag()
+  // }, [])
+  // console.log("infoTag", infoTag)
+  // did update => always run dont care anything
+  // useEffect(() =>{
+
+  // })
+
+  // didupdate follow dependency
+  // useEffect(() =>{
+
+  // },[dependency])
+
+  //  client side rendering ( CSR ) , server side rendering ( SSR )
+  // quy ước của BE có 4 errorCode phổ biến
+  // 1. errorCode = 200 => successful
+  // 2. 404 => not fround
+  // 3. 500 => something went wrong => api chết
+  // 4. 999 => invalid value => giá trị ko hợp lệ
+  //  es6 
+
+  // const renderRelatedTags = () => {
+  //   if (infoTag.relatedTags) {
+  //     return infoTag.relatedTags.map((relatedTag) => {
+  //       return <a href={`/vn/tags/${relatedTag.slug}`} title={relatedTag.name} key={relatedTag.id}>#{relatedTag.name}</a>
+  //     })
+  //   } else {
+  //     return null
+  //   }
+  // }
--- src/Components/ArticleCard/ArticleCard.jsx
+++ src/Components/ArticleCard/ArticleCard.jsx
@@ -1,22 +1,30 @@
-import "./ArticleCard.css"
+import "./ArticleCard.scss"
 
-export default function ArticleCard() {
+export default function ArticleCard({ articleDetails }) {
+    const HOST = "https://vietcetera.com/vn"
+
+    let dayTime = articleDetails.publishDate.slice(8, 10) + " THG " + articleDetails.publishDate.slice(5, 7);
+
+
     return (
         <div className="ArticleCardDetail">
-            <img className="ArticleImg" src="/articleImg1.png" alt="Hoiana" width="272" height="184" />
+            <div className="ImgContainer">
+                <a href={`${HOST}/${articleDetails.slug}`} title={articleDetails.title}>
+                    <img className="ArticleImg" src={`https://vietcetera.com/${articleDetails.images.url}`} alt="Hoiana" width="272" height="184" />   </a>
+            </div>
             <div className="ArticleContent">
-                <a className="ArticleTitle" href="/" title="Khám phá thế giới nghỉ dưỡng cao cấp tại khu phức hợp Hoiana">Khám phá thế giới nghỉ dưỡng cao cấp tại khu phức hợp Hoiana</a>
-                <p className="ArticleText">“Trứng, sữa đặc, bột cà phê, chút bơ, chút pho mát…nhưng tôi không thể nói tất cả được, bởi đây là công thức bí truyền của ...</p>
+                <a className="ArticleTitle" href={`${HOST}/${articleDetails.slug}`} title={articleDetails.title}>{articleDetails.title}</a>
+                <p className="ArticleText">{articleDetails.excerpt}</p>
                 <div className="ArticleDetailContainer">
                     <div className="ArticleDetail">
-                        <a href="/" title="Tai Vo">
-                            <img class="authorImg" src="/author1.png" alt="Tai Vo" width="32" height="32" />
+                        <a href={`${HOST}/thong-tin-ca-nhan/${articleDetails.writer.username}`} title={articleDetails.writer.penname}>
+                            <img class="authorImg" src={`https://vietcetera.com/${articleDetails.writer.avatar}`} alt={articleDetails.writer.penname} width="32" height="32" />
                         </a>
-                        <a className="authorName" href="/" title="Tai Vo">Tai Vo</a>
+                        <a className="authorName" href={`${HOST}/thong-tin-ca-nhan/${articleDetails.writer.username}`} title={articleDetails.writer.penname}>{articleDetails.writer.penname}</a>
                         <p>·</p>
-                        <p class="articleTime">4 Thg 5</p>
+                        <p class="articleTime">{dayTime}</p>
                         <p>·</p>
-                        <a className="articleCategories" href="/" title="Ở">Ở</a>
+                        <a className="articleCategories" href={`${HOST}/chu-de/${articleDetails.topic[0].key}`} title={articleDetails.topic[0].name}>{articleDetails.topic[0].name.toUpperCase()}</a>
 
                     </div>
                     <button className="saveBtn"> <img src="/saveBtn.png" alt="save" width="16" height="16" /></button>
